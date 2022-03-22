a5a5591883ebcf8a47a2e128145b7e3619ab877c
Shahzaib Arif <shahzaibarif6@gmail.com>
2022-03-22 12:15:21 +0500

    Cretaed

--- eTickets/Startup.cs
+++ eTickets/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using eTickets.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -24,6 +25,8 @@ namespace eTickets
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Dbcontext configuration
+            services.AddDbContext<AppDbContext>();
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
